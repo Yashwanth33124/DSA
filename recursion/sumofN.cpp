ba54dc80c704bb94a7f54aed4e408c43c62c2b79
Yashwanth Kumar <cheruvuyashwanth99@gmail.com>
2025-08-02 21:56:07 +0530

    adde

---
+++ recursion/sumofN.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void f(int i,int sum) {
+    if(i == 0) {
+        cout << sum << endl;
+        return;
+    }
+    f(i - 1,sum + i);
+}
+int main() {
+    int n;
+    cin >> n;
+    f(n,0);
+
+    return 0;
+}
+
